52d2357db9542c542ca25e607a931ad4e10960bd
qiuzp <qiuzp94@foxmail.com>
2020-02-27 19:16:06 +0800

    新增server.js代码与README说明

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # JSONP-demo
+## 预览方法
+### 第一步 克隆本项目到本地
+    > git@github.com:qiuzp94/JSONP-demo.git
+### 第二步 进入本地项目地址
+### 第三步 指定客户端与服务端的端口
+    > PORT=8001 node server.js
+    > PORT=8002 node server.js
+### 第四步 浏览器打开
+    
 ## 知识点
 ### addEventListener
 EventTarget.addEventListener()方法将指定的监听器注册到 EventTarget 上，当该对象触发指定的事件时，指定的回调函数就会被执行。 事件目标可以是一个文档上的元素 Element,Document和Window或者任何其他支持事件的对象 (比如 XMLHttpRequest)。
 
 https://developer.mozilla.org/zh-CN/docs/Web/API/EventTarget/addEventListeners
+
+## 什么是JSONP
+请求方：浏览器的前端程序员
+响应方：服务器的后端程序员
+
+请求方创建script,src指向响应方，同时传一个查询参数？callbackName=yyy
+响应方根据查询参数callBackName,构造形如
+1 yyy.call(undefined,'你要的数据')
+2 yyy('你要的数据)
+这样的响应
+浏览器接受到响应，就会执行yyy.call(undefined,'你要的数据')
+那么请求方就知道了他要的数据
+这就是JSONP
+
+## JSONP 为什么不支持 POST
+因为JSONP是通过动态创建script实现
+我们动态创建script时，只能用GET，没办法用POST
--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 var http = require('http')
-
 var fs = require('fs')
 var url = require('url')
 var port = process.env.PORT || 8888;
-var server = http.createServer(function (require, response) {
-    var temp = url.parse(require.url, true)
+
+var server = http.createServer(function (request, response) {
+    var temp = url.parse(request.url, true)
     var path = temp.pathname
     var query = temp.query
-    var method = require.mothod
+    var method = request.method
+
 
 
     //从这里开始看，上面不要看
-    
+
+    if (path === '/') {
+        var string = fs.readFileSync('./index.html', 'utf8')
+        var amount = fs.readFileSync('./db', 'utf8')
+        string = string.replace('&&&amount&&&', amount)
+        response.setHeader('Content-Type', 'text/html;charset-utf-8')
+        response.end(string)
+    } else if (path === '/style.css') {
+        var string = fs.readFileSync('./style.css')
+        response.setHeader('Content-Type', 'text/css')
+        response.end(string)
+    } else if (path === '/main.js') {
+        var string = fs.readFileSync('./main.js')
+        response.setHeader('Content-Type', 'applicatian/javascript')
+        response.end(string)
+    } else if (path === '/pay') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        var newAmount = amount - 1
+        fs.writeFileSync('./db', newAmount)
+        response.end(
+            `${query.callback}.call(undefined,'success')`
+        )
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset-utf-8')
+        response.end('找不到相应的路径，请您自行设置index.html')
+    }
+
+
 
     //代码结束，下面不要看
-    console.log(method + '' + require.url)
+    console.log(method + '' + request.url)
 
 })
 server.listen(port)
